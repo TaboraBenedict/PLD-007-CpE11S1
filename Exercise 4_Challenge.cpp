f6564fb36df0f58f2d8a66e8393824065d58b1b2
Tabora Benedict U. <qbutabora@tip.edu.ph>
2024-09-16 12:06:02 +0800

    Add files via upload

---
+++ Exercise 4_Challenge.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+	using namespace std;
+		int main()
+			{
+				int i=9;	
+				
+					do
+					{
+						cout<<"Hello World: "<<i<<endl;
+						i--;
+					}
+				while(i<=10&& i>=3);
+				return 0;
+			}
