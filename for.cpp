27e45fb7b3a996a6618cbffc9b3ed7cb44909833
Tabora Benedict U. <qbutabora@tip.edu.ph>
2024-09-16 11:29:32 +0800

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+	//Coded by Benedict Tabora 11:22 AM | 16/09/24
+	using namespace std;
+		int main()
+		{
+			//Data types and variables
+			int i=0;
+			int j=10;
+			
+				for(i=0;i<=j;i++)
+				
+				{
+					cout<<"Hello World: "<<i<<endl;
+				}
+			
+			//Process for each alteration and implement.
+			
+			//Alterations 0 where
+				//i=0,j=10,i=1  | 0<=10
+				//Hello World 0
+
+			//Alterations 1 where
+				//i=1,j=10,i=2  | 1<=10
+				//Hello World 1
+
+			//Alterations 2 where
+				//i=2,j=10,i=3  | 2<=10
+				//Hello World 2
+				
+			//Alterations 3 where
+				//i=3,j=10,i=4  | 3<=10
+				//Hello World 3
+
+				
+			//Alterations 4 where
+				//i=4,j=10,i=5  | 4<=10
+				//Hello World 4
+
+			//Alterations 5 where
+				//i=4,j=10,i=5  | 5<=10
+				//Hello World 5
+
+			//Alterations 6 where
+				//i=5,j=10,i=6  | 6<=10
+				//Hello World 6
+				
+			//Alterations 7 where
+				//i=6,j=10,i=7  | 7<=10
+				//Hello World 7
+
+			//Alterations 8 where
+				//i=7,j=10,i=8  | 8<=10
+				//Hello World 8
+
+			//Alterations 9 where
+				//i=8,j=10,i=9  | 9<=10
+				//Hello World 9
+				
+			//Alterations 10 where
+				//i=9,j=10,i=10  | 10<=10
+				//Hello World 10
+		
+			return 0;
+		}
+		
