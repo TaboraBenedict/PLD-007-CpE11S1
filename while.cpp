60c3396c485e952d0eac7678ca91da43527ae2c4
Tabora Benedict U. <qbutabora@tip.edu.ph>
2024-09-16 11:30:00 +0800

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+	//Coded by Benedict Tabora 11:22 AM | 16/09/24
+	using namespace std;
+		int main()
+		
+		{
+			int i=0;
+				
+				while(i<=10)
+				{
+					cout<<"Hello World: "<<i<<endl;
+					i=i+1;
+				}
+		return 0;
+	}
