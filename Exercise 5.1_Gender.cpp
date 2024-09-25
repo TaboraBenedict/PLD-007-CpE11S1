9c30aad7a2cf397a762766011436e2b65a4a09d8
Tabora Benedict U. <taborabenedict47@gmail.com>
2024-09-25 12:46:56 +0800

    Exercise 5.1_Gender.cpp

--- Exercise 5.1_Gender.cpp
+++ Exercise 5.1_Gender.cpp
@@ -4,13 +4,9 @@
 			{
 				//chars
 				char g;
-				char a;
 				
 					cout<<"Kindly input your gender"<<endl;
 					cin>>g;
-										
-					cout<<"Kindly input your age"<<endl;
-					cin>>a;
 				
 				switch (g)
 					{
@@ -37,51 +33,6 @@
 					cout<<"What?? kindly re-run the program and try again"<<endl;
 					break;
 				}
-
-				switch (a)
-					{
-						case '1':
-						cout<<"Accepted!"<<endl;
-						break;
-						
-						case '2':
-						cout<<"Accepted!"<<endl;
-						break;
-						
-						case '3':
-						cout<<"Accepted!"<<endl;
-						break;
-						
-						case '4':
-						cout<<"Accepted!"<<endl;
-						break;
-						
-						case '5':
-							cout<<"Accepted!"<<endl;
-								break;
-						
-						case '6':
-							cout<<"Accepted!"<<endl;
-								break;
-								
-						case '7':
-							cout<<"Accepted!"<<endl;
-								break;
-								
-						case '8':
-							cout<<"Accepted!"<<endl;
-								break;
-						
-						case '9':
-							cout<<"Accepted!"<<endl;
-								break;
-						
-					default:
-						{
-							cout<<"Retry again"<<endl;
-								break;
-						}
-					}
 			}
 		return 0;
 }
