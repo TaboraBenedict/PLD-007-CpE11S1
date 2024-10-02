bf0cce040022f79cc4dfeaccf026e77d68816c3e
Tabora Benedict U. <qbutabora@tip.edu.ph>
2024-10-02 12:54:05 +0800

    Add files via upload

---
+++ Practical Lab.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+		using namespace std;
+			int main()
+			
+			{
+				
+				int n1,n2,n3,n4,n5,s;
+				
+				cout<<"Input 5 numbers"<<endl;
+				cin>>n1>>n2>>n3>>n4>>n5;
+				
+				while(n1>=n5);
+				{
+					s=n1+n5;
+						cout<<"The value of first and last number combined is: "<<s<<endl;
+				}
+	return 0;
+}
