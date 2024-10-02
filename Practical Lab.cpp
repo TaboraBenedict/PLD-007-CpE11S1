131f50c9386573816327666eeb9d97c93e1aac07
Tabora Benedict U. <taborabenedict47@gmail.com>
2024-10-02 20:32:44 +0800

    Practical Lab.cpp

--- Practical Lab.cpp
+++ Practical Lab.cpp
@@ -6,13 +6,16 @@
 				
 				int n1,n2,n3,n4,n5,s;
 				
+				do
+				{
+				
 				cout<<"Input 5 numbers"<<endl;
 				cin>>n1>>n2>>n3>>n4>>n5;
 				
-				while(n1>=n5);
-				{
+
 					s=n1+n5;
 						cout<<"The value of first and last number combined is: "<<s<<endl;
 				}
+				while(true);
 	return 0;
 }
