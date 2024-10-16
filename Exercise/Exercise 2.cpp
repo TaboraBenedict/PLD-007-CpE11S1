d3224c3ebd6ca169a530e5a0fd5f89d109eae53c
Tabora Benedict U. <taborabenedict47@gmail.com>
2024-10-16 12:42:27 +0800

    Create Exercise 2.cpp

---
+++ Exercise/Exercise 2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+ using namespace std;
+ 	
+	int main()
+	
+	{
+		//local variables
+		int x=4,y=2,s=0,d=0,p=0,q=0,m=0;
+		
+		cout<<"The value of x is 4"<<endl;
+			cout<<"The value of y is 2"<<endl;
+				cout<<endl;
+			
+		 	s = (x+y);
+		  		d = (x-y);
+		   			p = (x*y);
+		    			q = (x/y);
+		     				m = (x%y);
+		     
+
+		 				cout<<"The sum is: "<<s<<endl;
+		 			cout<<"The difference is: "<<d<<endl;
+		 		cout<<"The product is: "<<p<<endl;
+			cout<<"The quotient is: "<<q<<endl;
+		cout<<"The remainder is: "<<m<<endl;
+		
+		return 0;
+	}
