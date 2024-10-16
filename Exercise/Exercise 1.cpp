d50b88ea8fe298159a087dfb3bfd6d28c4501fc7
Tabora Benedict U. <taborabenedict47@gmail.com>
2024-10-16 12:42:04 +0800

    Create Exercise 1.cpp

---
+++ Exercise/Exercise 1.cpp
@@ -0,0 +1,38 @@
+---Exercise I | August 14, 2024
+
+1. What is Computer? 
+--A Computer is a machinery electronic device that we tend to use in this modern world. The creation of the computer goes way back in 19th century. 
+A computer is a bundle like device that also comes with many parts, not just one but alot, you can include the mouse, keyboard, RAM, CPU, PSU (Power Supply Unit) and more.
+
+2. What are the essential parts of computer?
+--The essential parts of a computer are motherboard, hard disk drive, processing unit, power supply unit, and graphics card or graphic processing unit. These are one of the main essential parts to make a single computer functionable. Each parts has its own
+functions inside the computer, for instances, the motherboard, which is a part of a computer that ensures your computer's stability for its everyday use.
+
+3. Who are the notable inventors of hardware and software? (Cite at least (5) Software developers and (5) Hardware developers.
+  --Software Developers
+--Bill Gates was one of the notable inventors back then for his amazing accomplishments in bring computer to life. 
+--Steve Jobs was a successful software developer of Apple, the apple app that people still use to this day.
+--Linus Torvalds is one of the successful developers in the world, he created the Linux Kernel, which is still used worldwide. His work has revolutionized the way of managing source codes and became the most used for software developers, who find the linux kernel 
+  way more efficient.
+--Guido van Rossum was one of the notable inventors in Software, he is the creator of python, a programming language that is widely used and commonly known amongst programmers.
+--Mark Zuckerberg was a software developer and became known for creating the social media called Facebook. He revolutionized the way of socializing, contacting, messsaging and way to interact to others.
+
+  --Hardware Developers
+--APPLE was created by Steve jobs with the help of his co-founders, Steve Wozniak and Ronald Wayne, who was also the first person to leave the company for personal reasons. His inventions was an Iphone, Ipad, and Macbook
+--LENOVO is a hardware developed by Liu Chuanzhi, together with his colleagues. He sold a couple of PC under the brand of the given name he invented, LENOVO and was recognized by other consumers and quickly gained their reputation.
+--ASUS was founded by  T.H. Tung, Ted Hsu, Wayne Hsieh and M.T Liao. ASUS was known for their best selling inventions like a laptop under their brand.
+--NVIDIA was founded by Jenseng Huang. His work has revolutionized the GPU (Graphics Processing Unit) inlcuding his latest inventions, GeForce together with the help of artificial intelligence, he was able to fully enhance his work to a whole new degree.
+--Koo In-hwoi is the current founder LG Electronics was reinstotuted in 2002, Their inventions are Air conditioning, Television and other smart appliances.
+
+4. REFERENCES
+-FirmsTalk. (2024, March 25). Top 10 Software Developers in the World (2024 Updated List). https://www.linkedin.com/pulse/top-10-software-developers-world-2024-updated-list-firmstalk-jbepf
+-Strydom, M. (2024, July 14). Parts Of A Computer And Their Functions (All Components). Computer Info Bits. https://computerinfobits.com/parts-of-computer-and-their-functions/
+-Khanachivskyi, O., & Litslink. (2024, June 18). Top 10 Software Developers of All Time and Their Innovations. Litslink. https://litslink.com/blog/top-10-software-developers-of-all-time
+-Bill Gates. (n.d.). Forbes. https://www.forbes.com/profile/bill-gates/
+-Mark Zuckerberg. (2019, October 24). Biography. https://www.biography.com/business-leaders/mark-zuckerberg
+-Lenovo Group Ltd. (n.d.). Nikkei Asia. https://asia.nikkei.com/Companies/Lenovo-Group-Ltd#:~:text=Company%20Background&text=Founded%20by%20Liu%20Chuanzhi%20and,than%20those%20of%20its%20rivals.
+-Wiki, C. T. A. (n.d.). Ronald Wayne. Apple Wiki. https://apple.fandom.com/wiki/Ronald_Wayne
+-Asus History: Founding, Timeline, and Milestones - Zippia. (2023, July 21). https://www.zippia.com/asus-careers-1547151/history/
+-Publishing, K. (2024, March 18). Steve Job - Founder of Apple Inc. https://www.linkedin.com/pulse/steve-job-founder-apple-inc-kharis-publishing-bqhsf
+-What’s New at NVIDIA. (n.d.). NVIDIA. https://www.nvidia.com/en-us/about-nvidia/board-of-directors/jensen-huang/
+-About LG > History. (n.d.). https://www.lgcorp.com/about/history/1
